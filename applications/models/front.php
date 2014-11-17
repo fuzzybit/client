91be82adeb2547fb9fa474a5298a522c23efbdbe
atucovic <fuzzybit@users.noreply.github.com>
2014-11-17 15:22:11 -0600

    removed CORS header

--- applications/models/front.php
+++ applications/models/front.php
@@ -142,7 +142,6 @@
 		 */
 		private function __construct()
 		{
-			header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH, OPTIONS");
 			header("Access-Control-Allow-Headers: X-Requested-With");
 			header("Access-Control-Allow-Credentials: true");
 
